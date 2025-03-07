70fcc13f3af11d42ea8e5a0fe28aa7776af1c210
richardvalga <130559057+richardvalga@users.noreply.github.com>
2025-03-07 18:08:01 +0100

    Add files via upload

--- ipfs_scraper.py
+++ ipfs_scraper.py
@@ -1,12 +1,14 @@
 import requests
 import json
 
-save_to_path = 'ace_ids.txt'
 
 def get_acelinks_ipfs():
 
-    url = 'https://ipfs.io/ipns/elcano.top'
-
+    # NEW LIST??
+    url = 'https://ipfs.io/ipns/k51qzi5uqu5dgg9al11vomikugim0o1i3l3fxp3ym3jwaswmy9uz8pq4brg1u9'
+    #url = 'https://ipfs.io/ipns/elcano.top'
+    # OLD STATIC LIST
+    #url = 'https://bafybeihaak6jp7c6y3weyynkfruefwtgchvx2uuppmtts775k4z7kyw4ny.ipfs.dweb.link'
     '''
     # TOR USAGE. UNCOMMENT LINE INSIDE try/except BOLOCK:
     proxies = {
@@ -23,9 +25,11 @@ def get_acelinks_ipfs():
         # Send your bot notification here
         print(f"IPFS ERROR: {e}")
         exit(1)
+        return
 
     print("IPFS Response Status:", response.status_code)
     content = response.content.decode('utf-8', errors='replace')  # Use UTF-8 encoding
+    #print(content)
 
     start_marker = "const linksData = "
     start_index = content.find(start_marker)
@@ -40,6 +44,7 @@ def get_acelinks_ipfs():
             if not links_data["links"]:
                 print("No links found in links_data.")
                 exit(1)
+                return
             plain_text_list = []
 
             for link in links_data["links"]:
@@ -47,19 +52,21 @@ def get_acelinks_ipfs():
                     continue
                 plain_text_list.append(link["name"])
                 #print(link["name"])
-                plain_text_list.append(link["url"])
+                plain_text_list.append(link["url"].replace("acestream://", ""))  # Strip "acestream://"
                 #print(link["url"].replace("acestream://", ""))
 
-            with open(save_to_path, 'w', encoding='utf-8') as file:
+            with open('toys/cachedList.txt', 'w', encoding='utf-8') as file:
                 for item in plain_text_list:
                     file.write(f"{item}\n")
 
             saved_link_count = len(plain_text_list) // 2
             print(f"{saved_link_count} enlaces acestream guardados desde IPFS")
+            #exit(0)
 
     else:
         print("No links data found in the content.")
         exit(1)
+        return
 
 
-get_acelinks_ipfs()
+#get_acelinks_ipfs()
